735b86734c6560e6d252a7451f7bf2cd3e7195e8
Mario Arguello Borge <46728059+Mariozdz@users.noreply.github.com>
2021-06-13 15:12:58 -0600

    Update swipe with data

--- Front-End/app/src/main/java/una/moviles/ui/checkout/CheckoutFragment.kt
+++ Front-End/app/src/main/java/una/moviles/ui/checkout/CheckoutFragment.kt
@@ -1,17 +1,20 @@
 package una.moviles.ui.checkout
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import una.moviles.R
 import una.moviles.RegisterActivity
 import una.moviles.databinding.FragmentCheckoutBinding
 import una.moviles.databinding.FragmentHomeBinding
+import una.moviles.logic.Flight
 import una.moviles.persistence.BD
 import una.moviles.ui.home.HomeAdapter
 import una.moviles.ui.home.HomeViewModel
@@ -48,6 +51,17 @@ class CheckoutFragment : Fragment() {
 
     override fun onResume() {
         super.onResume()
+
+        var bund =  requireArguments().getSerializable("flight") as Flight
+
+        Toast.makeText(context, "" + bund.destino, Toast.LENGTH_SHORT).show()
+
+
+            if (bund != null) {
+
+                binding.editform = bund
+            }
+
 
 
     }
--- Front-End/app/src/main/java/una/moviles/ui/home/HomeFragment.kt
+++ Front-End/app/src/main/java/una/moviles/ui/home/HomeFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.ItemTouchHelper
@@ -91,8 +92,10 @@ class HomeFragment : Fragment() {
                 var quien: String = ""
 
                 if (direction == ItemTouchHelper.LEFT) {
-                    
-                    view!!.findNavController().navigate(R.id.nav_check)
+
+
+                    val bundle = bundleOf("flight" to BD.flight.value!![viewHolder.adapterPosition] )
+                    view!!.findNavController().navigate(R.id.nav_check,bundle)
 
                 } else {
 
